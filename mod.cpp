9a8c925fc8df97bd2d9d94a2c4c9c07c36425ec8
ZA-Wolf <47332557+ZA-Wolf@users.noreply.github.com>
2019-03-31 17:05:26 +0200

    Updated with group data

--- mod.cpp
+++ mod.cpp
@@ -1,7 +1,7 @@
-name = "NIArsenal";
-picture = "NIArms1_ca.paa";
+name = "Zulu-Alpha Weapons Pack";
+picture = "";
 hidePicture = "0";
 hideName = "0";
-actionName = "Website";
-action = "http://credmo.updatedtuesdays.com/tier1";
-description = "NIArsenal";
+actionName = "Zulu-Alpha Website";
+action = "http://zulu-alpha.co.za";
+description = "Zulu-Alpha Weapons Pack";
