f64c6bef45688b95a865d86c81aa4e83ba3dc550
ZA-Wolf <47332557+ZA-Wolf@users.noreply.github.com>
2019-04-06 18:04:04 +0200

    Added images and editing mod.cpp accordingly

--- mod.cpp
+++ mod.cpp
@@ -1,7 +1,17 @@
 name = "Zulu-Alpha Weapons Pack";
-picture = "";
-hidePicture = "0";
-hideName = "0";
-actionName = "Zulu-Alpha Website";
-action = "http://zulu-alpha.co.za";
-description = "Zulu-Alpha Weapons Pack";
+picture = "Data\picture.paa";
+logoSmall = "Data\logo_small.paa";
+logo = "Data\logo.paa";
+logoOver = "Data\logo_over.paa";
+action = "https://github.com/zulu-alpha/mod-lines";
+tooltipOwned = "Zulu-Alpha - Test Mod Line";
+dlcColor[] = 
+{
+	0.26,
+	0.17,
+	0.12,
+	1
+};
+overview = "The purpose of this mod is to have all the mods of the Zulu-Alpha - Test Mod Line as dependencies in the Steam Workshop";
+hideName = 0;
+hidePicture = 0;
